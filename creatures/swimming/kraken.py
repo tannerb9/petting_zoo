056329fd563d7a4495336253dc00629473f8db94
tannerb9 <tanner-brainard@hotmail.com>
2020-07-16 21:57:08 -0500

    Modularized code from animals.py. Created index.py and moved logic there. Refactored attractions and creatures to one class each.

---
+++ creatures/swimming/kraken.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+
+class Kraken:
+
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+        self.food = food
+
+    def feed(self):
+        print(f"{self.name} was fed {self.food} on {date.today()}.")
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}."
