394b11422978559580a84f85a90b4d75141a99aa
tannerb9 <tanner-brainard@hotmail.com>
2020-07-20 15:15:59 -0500

    Created classes Land/Air/WaterCreature and implemented inheritance from parent Creature. Overrode feed func of parent class in each child class to print different message.

--- beasts.py
+++ beasts.py
@@ -24,3 +24,35 @@ class Creature:
 
     def __str__(self):
         return f"{self.name} is a {self.species}."
+
+
+class AirCreature(Creature):
+
+    def __init__(self, name, species, food, chip_num, **shift):
+        super().__init__(name, species, food, chip_num, **shift)
+        self.flying = True
+
+    def feed(self):
+        print(
+            f"{self.name} swooped through the air to catch the {self.food} for their meal on {date.today()}.")
+
+
+class WaterCreature(Creature):
+
+    def __init__(self, name, species, food, chip_num, **shift):
+        super().__init__(name, species, food, chip_num, **shift)
+        self.swimming = True
+
+    def feed(self):
+        print(f"{self.name} tossed their {self.food} into the air for fun before devouring it on {date.today()}.")
+
+
+class LandCreature(Creature):
+
+    def __init__(self, name, species, food, chip_num, **shift):
+        super().__init__(name, species, food, chip_num, **shift)
+        self.walking = True
+
+    def feed(self):
+        print(
+            f"{self.name} frightened the {self.food} so they could chase it down on {date.today()}.")
